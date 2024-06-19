e2f05a79f5523b4640b796a5c1e7a0c62d6c8ff1
Slime-Inc7 <meta94in@gmail.com>
2024-06-20 08:51:55 +0900

    Update index.js

--- es/ConfigProvider/index.js
+++ es/ConfigProvider/index.js
@@ -17,6 +17,9 @@ export var useCdnFn = function useCdnFn() {
     var pkg = _ref2.pkg,
         version = _ref2.version,
         path = _ref2.path;
+    if (pkg === '@lobehub/assets-logo') {  // 로고 패키지일 경우 직접 URL 반환
+      return 'https://cafeptthumb-phinf.pstatic.net/MjAyNDA2MThfMjY5/MDAxNzE4NjcwMDk2NzEx.MtcQUfcarXwhmMlj06k7-ikFrhsDTdH_wJe_tlQ6qoYg.T38Yqxbtjyc00axRtMCNnET8BCXgppAshnbjnxTEc20g.PNG/slime.png?type=w1600';
+    }
     return genCdnUrl({
       path: path,
       pkg: pkg,
@@ -28,6 +31,9 @@ export var useCdnFn = function useCdnFn() {
     var pkg = _ref3.pkg,
         version = _ref3.version,
         path = _ref3.path;
+    if (pkg === '@lobehub/assets-logo') {  // 로고 패키지일 경우 직접 URL 반환
+      return 'https://cafeptthumb-phinf.pstatic.net/MjAyNDA2MThfMjY5/MDAxNzE4NjcwMDk2NzEx.MtcQUfcarXwhmMlj06k7-ikFrhsDTdH_wJe_tlQ6qoYg.T38Yqxbtjyc00axRtMCNnET8BCXgppAshnbjnxTEc20g.PNG/slime.png?type=w1600';
+    }
     return genCdnUrl({
       path: path,
       pkg: pkg,
@@ -39,6 +45,9 @@ export var useCdnFn = function useCdnFn() {
     var pkg = _ref4.pkg,
         version = _ref4.version,
         path = _ref4.path;
+    if (pkg === '@lobehub/assets-logo') {  // 로고 패키지일 경우 직접 URL 반환
+      return 'https://cafeptthumb-phinf.pstatic.net/MjAyNDA2MThfMjY5/MDAxNzE4NjcwMDk2NzEx.MtcQUfcarXwhmMlj06k7-ikFrhsDTdH_wJe_tlQ6qoYg.T38Yqxbtjyc00axRtMCNnET8BCXgppAshnbjnxTEc20g.PNG/slime.png?type=w1600';
+    }
     return genCdnUrl({
       path: path,
       pkg: pkg,
