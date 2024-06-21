e30753e11344384b389b6b96850226f3ac44bfba
Slime-Inc7 <meta94in@gmail.com>
2024-06-21 17:08:46 +0900

    Update LogoSpline.js

--- es/LogoThree/LogoSpline.js
+++ es/LogoThree/LogoSpline.js
@@ -4,12 +4,12 @@ import _objectWithoutProperties from "@babel/runtime/helpers/esm/objectWithoutPr
 var _excluded = ["className", "style", "width", "height", "onLoad"];
 import { useThemeMode } from 'antd-style';
 import { memo, useState } from 'react';
-import Spline from "../Spline";
-import Loading from "./Loading";
 import { jsx as _jsx } from "react/jsx-runtime";
 import { jsxs as _jsxs } from "react/jsx-runtime";
-var LIGHT = 'https://gw.alipayobjects.com/os/kitchen/J9jiHITGrs/scene.splinecode';
-var DARK = 'https://gw.alipayobjects.com/os/kitchen/CzQKKvSE8a/scene.splinecode';
+
+// 새로운 이미지 URL을 추가합니다.
+var IMAGE_URL = 'https://raw.githubusercontent.com/Slime-Inc7/ui/master/logo-3d.png';
+
 var LogoSpline = /*#__PURE__*/memo(function (_ref) {
   var className = _ref.className,
     style = _ref.style,
@@ -23,20 +23,38 @@ var LogoSpline = /*#__PURE__*/memo(function (_ref) {
     _useState2 = _slicedToArray(_useState, 2),
     loading = _useState2[0],
     setLoading = _useState2[1];
+
+  // 새로운 스타일 객체 추가
+  const containerStyle = {
+    height: height,
+    width: width,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',         // flexbox 사용
+    alignItems: 'center',    // 수직 중앙 정렬
+    justifyContent: 'center', // 수평 중앙 정렬
+    ...style                 // 기존 스타일 병합
+  };
+
+  const imgStyle = {
+    maxHeight: '100%',
+    maxWidth: '100%'
+  };
+
   return /*#__PURE__*/_jsxs("div", {
     className: className,
-    style: _objectSpread({
-      height: height,
-      position: 'relative',
-      width: width
-    }, style),
-    children: [loading && /*#__PURE__*/_jsx(Loading, {}), /*#__PURE__*/_jsx(Spline, _objectSpread({
-      onLoad: function onLoad(splineApp) {
-        setLoading(false);
-        _onLoad === null || _onLoad === void 0 || _onLoad(splineApp);
-      },
-      scene: isDarkMode ? DARK : LIGHT
-    }, rest))]
+    style: containerStyle,  // 수정된 스타일 사용
+    children: [loading && /*#__PURE__*/_jsx(Loading, {}), 
+      // Spline 대신 img 태그를 사용합니다.
+      /*#__PURE__*/_jsx("img", {
+        src: IMAGE_URL,
+        alt: "Logo",
+        onLoad: function onLoad() {
+          setLoading(false);
+          _onLoad === null || _onLoad === void 0 || _onLoad();
+        },
+        style: imgStyle  // 이미지 스타일 적용
+      }, rest)]
   });
 });
 export default LogoSpline;
