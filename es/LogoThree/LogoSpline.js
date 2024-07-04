4f0c6182eb34eda1203ce202cc388863e0aa6efd
Slime-Inc7 <meta94in@gmail.com>
2024-07-04 23:52:07 +0900

    Update LogoSpline.js

--- es/LogoThree/LogoSpline.js
+++ es/LogoThree/LogoSpline.js
@@ -1,14 +1,13 @@
 import _objectSpread from "@babel/runtime/helpers/esm/objectSpread2";
-import _slicedToArray from "@babel/runtime/helpers/esm/slicedToArray";
 import { memo, useState, useCallback, CSSProperties } from 'react';
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 
 const LOGO_URL = 'https://raw.githubusercontent.com/Slime-Inc7/ui/master/logo-3d.png';
 
 const LogoSpline = /*#__PURE__*/memo(function ({ className, style, width = 150, height = 150 }) {
-  const [_useState, setUseState] = useState(true); // 수정된 부분
+  const [loading, setLoading] = useState(true); // 수정된 부분
   const handleLoad = useCallback(() => {
-    setUseState(false);
+    setLoading(false);
   }, []);
 
   // width와 height를 숫자형 값으로 변환하여 NaN이 발생하지 않도록 함
@@ -24,7 +23,7 @@ const LogoSpline = /*#__PURE__*/memo(function ({ className, style, width = 150, 
 
   return /*#__PURE__*/_jsxs("div", {
     children: [
-      _useState && /*#__PURE__*/_jsx("img", {
+      loading && /*#__PURE__*/_jsx("img", {
         alt: 'logo',
         height: computedHeight * 0.75,
         onLoad: handleLoad,
@@ -34,7 +33,7 @@ const LogoSpline = /*#__PURE__*/memo(function ({ className, style, width = 150, 
         },
         width: computedWidth * 0.75
       }),
-      !_useState && /*#__PURE__*/_jsx("img", {
+      !loading && /*#__PURE__*/_jsx("img", {
         alt: 'logo',
         height: computedHeight,
         src: LOGO_URL,
