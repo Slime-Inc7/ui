884e2a5800c21701680a0d190e7daa24bb867eac
Slime-Inc7 <meta94in@gmail.com>
2024-07-04 23:09:05 +0900

    Update LogoSpline.js

--- es/LogoThree/LogoSpline.js
+++ es/LogoThree/LogoSpline.js
@@ -1,22 +1,19 @@
 import _objectSpread from "@babel/runtime/helpers/esm/objectSpread2";
-import _slicedToArray from "@babel/runtime/helpers/esm/slicedToArray";
 import { memo, useState, useCallback } from 'react';
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 
 const LOGO_URL = 'https://raw.githubusercontent.com/Slime-Inc7/ui/master/logo-3d.png';
 
-const LogoSpline = /*#__PURE__*/memo(function (_ref) {
-  const { className, style, width, height } = _ref;
-  const [_useState, _useState2] = _slicedToArray(useState(true), 2);
-  const [loading, setLoading] = _slicedToArray(useState(true), 2);
+const LogoSpline = /*#__PURE__*/memo(function ({ className, style, width = '150', height = '150' }) {
+  const [loading, setLoading] = useState(true);
 
   const handleLoad = useCallback(() => {
     setLoading(false);
   }, []);
 
   // width와 height를 숫자형 값으로 변환하여 NaN이 발생하지 않도록 함
-  const computedWidth = typeof width === 'number' ? width : parseFloat(width) || 0;
-  const computedHeight = typeof height === 'number' ? height : parseFloat(height) || 0;
+  const computedWidth = parseFloat(width) || 150;
+  const computedHeight = parseFloat(height) || 150;
 
   return /*#__PURE__*/_jsxs("div", {
     children: [
