3e30c910e3b54f19080ee8a37a0ee1c98bd93569
Slime-Inc7 <meta94in@gmail.com>
2024-07-04 23:38:30 +0900

    Update LogoSpline.js

--- es/LogoThree/LogoSpline.js
+++ es/LogoThree/LogoSpline.js
@@ -1,23 +1,30 @@
 import _objectSpread from "@babel/runtime/helpers/esm/objectSpread2";
-import { memo, useState, useCallback } from 'react';
+import _slicedToArray from "@babel/runtime/helpers/esm/slicedToArray";
+import { memo, useState, useCallback, CSSProperties } from 'react';
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 
 const LOGO_URL = 'https://raw.githubusercontent.com/Slime-Inc7/ui/master/logo-3d.png';
 
-const LogoSpline = /*#__PURE__*/memo(function ({ className, style, width = '150', height = '150' }) {
-  const [loading, setLoading] = useState(true);
-
+const LogoSpline = /*#__PURE__*/memo(function ({ className, style, width = 150, height = 150 }) {
+  const [_useState, setUseState] = useState(true); // 수정된 부분
   const handleLoad = useCallback(() => {
-    setLoading(false);
+    setUseState(false);
   }, []);
 
   // width와 height를 숫자형 값으로 변환하여 NaN이 발생하지 않도록 함
-  const computedWidth = parseFloat(width) || 150;
-  const computedHeight = parseFloat(height) || 150;
+  const computedWidth = parseFloat(width.toString()) || 150;
+  const computedHeight = parseFloat(height.toString()) || 150;
+
+  const containerStyle: CSSProperties = {
+    height: computedHeight,
+    width: computedWidth,
+    position: 'relative',
+    ...style
+  };
 
   return /*#__PURE__*/_jsxs("div", {
     children: [
-      loading && /*#__PURE__*/_jsx("img", {
+      _useState && /*#__PURE__*/_jsx("img", {
         alt: 'logo',
         height: computedHeight * 0.75,
         onLoad: handleLoad,
@@ -27,7 +34,7 @@ const LogoSpline = /*#__PURE__*/memo(function ({ className, style, width = '150'
         },
         width: computedWidth * 0.75
       }),
-      !loading && /*#__PURE__*/_jsx("img", {
+      !_useState && /*#__PURE__*/_jsx("img", {
         alt: 'logo',
         height: computedHeight,
         src: LOGO_URL,
@@ -39,11 +46,7 @@ const LogoSpline = /*#__PURE__*/memo(function ({ className, style, width = '150'
       })
     ],
     className: className,
-    style: _objectSpread({
-      height: computedHeight,
-      position: 'relative',
-      width: computedWidth
-    }, style)
+    style: containerStyle
   });
 });
 
